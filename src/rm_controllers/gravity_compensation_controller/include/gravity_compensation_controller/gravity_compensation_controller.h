54820adf91c0040062c2a787c05d4dddce33d567
OceanCing <3419311969@qq.com>
2026-05-27 14:15:01 +0000

    Update gravity compensation controller.

--- src/rm_controllers/gravity_compensation_controller/include/gravity_compensation_controller/gravity_compensation_controller.h
+++ src/rm_controllers/gravity_compensation_controller/include/gravity_compensation_controller/gravity_compensation_controller.h
@@ -2,10 +2,16 @@
 
 #include <ros/ros.h>
 #include <joint_trajectory_controller/joint_trajectory_controller.h>
-#include "hardware_interface/joint_command_interface.h"
-#include "hardware_interface/robot_hw.h"
-#include "trajectory_interface/trajectory_interface.h"
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/robot_hw.h>
+#include <trajectory_interface/trajectory_interface.h>
 #include <trajectory_interface/quintic_spline_segment.h>
+#include <urdf/model.h>
+#include <kdl_parser/kdl_parser.hpp>
+#include <kdl/tree.hpp>
+#include <kdl/chain.hpp>
+
+#include <map>
 
 namespace gravity_compensation_controller
 {
@@ -19,8 +25,16 @@ public:
   bool init(hardware_interface::EffortJointInterface* hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh) override;
 
   void update(const ros::Time& time, const ros::Duration& period) override;
+
+private:
+  std::vector<double> gravity_torques_;
+  std::vector<double> offset_torques_;
+  urdf::Model model_;
+  KDL::Tree tree_;
+  KDL::Chain chain_;
+  std::map<std::string, hardware_interface::JointStateHandle> joint_states_;
 };
-}
+} // namespace gravity_compensation_controller
 
 int main(){
   gravity_compensation_controller::GravityCompensationController ctrl;
---
+++ src/rm_controllers/gravity_compensation_controller/src/gravity_compensation_controller.cpp
@@ -0,0 +1,10 @@
+#include "gravity_compensation_controller/gravity_compensation_controller.h"
+
+namespace gravity_compensation_controller
+{
+bool GravityCompensationController::init(hardware_interface::EffortJointInterface *hw, ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh)
+{
+  
+}
+
+}
