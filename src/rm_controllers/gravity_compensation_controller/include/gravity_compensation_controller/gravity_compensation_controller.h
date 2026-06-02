415a863fbc30db3039b39d26ebf9ab6e5de96bef
OceanCing <3419311969@qq.com>
2026-06-02 14:03:56 +0000

    Complete GravityCompensationController and deploy on robot with manual.

--- src/rm_controllers/gravity_compensation_controller/include/gravity_compensation_controller/gravity_compensation_controller.h
+++ src/rm_controllers/gravity_compensation_controller/include/gravity_compensation_controller/gravity_compensation_controller.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <ros/ros.h>
+#include <std_srvs/SetBool.h>
 #include <joint_trajectory_controller/joint_trajectory_controller.h>
+#include <trajectory_msgs/JointTrajectory.h>
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/robot_hw.h>
 #include <trajectory_interface/trajectory_interface.h>
@@ -15,36 +17,39 @@
 #include <kdl/chaindynparam.hpp>
 
 #include <map>
+#include <atomic>
 
 namespace gravity_compensation_controller
 {
-class GravityCompensationController : public joint_trajectory_controller::JointTrajectoryController<
+class GravityCompensationJointTrajectoryController : public joint_trajectory_controller::JointTrajectoryController<
   trajectory_interface::QuinticSplineSegment<double>,
   hardware_interface::EffortJointInterface>
 {
 public:
-  GravityCompensationController() : JointTrajectoryController() {}
+  GravityCompensationJointTrajectoryController() : JointTrajectoryController() {}
 
   bool init(hardware_interface::EffortJointInterface* hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh) override;
 
   void update(const ros::Time& time, const ros::Duration& period) override;
+
+  bool positionHoldCallback(std_srvs::SetBool::Request& req, std_srvs::SetBool::Response& res);
 
 private:
   void computeGravityTorques();
 
   std::vector<double> gravity_torques_;
   std::vector<double> offset_torques_;
+  std::vector<double> tau_g_scale_;
   std::string chain_root_,chain_tip_;
   urdf::Model model_;
   KDL::Tree tree_;
   KDL::Chain chain_;
   std::unique_ptr<KDL::ChainDynParam> solver_;
   KDL::JntArray joint_positions_;
+  KDL::JntArray tau_g_;
+  std::atomic<bool> position_hold_{false};
+  bool last_position_hold_ = false;
+  ros::ServiceServer pos_hold_server_;
   // std::map<std::string, hardware_interface::JointStateHandle> joint_states_;
 };
 } // namespace gravity_compensation_controller
-
-int main(){
-  gravity_compensation_controller::GravityCompensationController ctrl;
-  
-}
--- src/rm_controllers/gravity_compensation_controller/src/gravity_compensation_controller.cpp
+++ src/rm_controllers/gravity_compensation_controller/src/gravity_compensation_controller.cpp
@@ -1,13 +1,15 @@
 #include "gravity_compensation_controller/gravity_compensation_controller.h"
+#include <kdl/chain.hpp>
 #include <kdl/chaindynparam.hpp>
 #include <kdl/jntarray.hpp>
+#include <kdl/tree.hpp>
 #include <memory>
-#include "XmlRpcValue.h"
-#include "kdl_parser/kdl_parser.hpp"
+#include <XmlRpcValue.h>
+#include <pluginlib/class_list_macros.hpp>
 
 namespace gravity_compensation_controller
 {
-bool GravityCompensationController::init(hardware_interface::EffortJointInterface *hw, ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh)
+bool GravityCompensationJointTrajectoryController::init(hardware_interface::EffortJointInterface *hw, ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh)
 {
   if (!JointTrajectoryController::init(hw, root_nh, controller_nh))
   {
@@ -29,6 +31,23 @@ bool GravityCompensationController::init(hardware_interface::EffortJointInterfac
       return false;
     }
     offset_torques_.push_back(static_cast<double>(offset_torques[i]));
+  }
+
+  XmlRpc::XmlRpcValue tau_g_scale;
+  controller_nh.param("tau_g_scale", tau_g_scale, {0.0});
+  if (tau_g_scale.getType() != XmlRpc::XmlRpcValue::TypeArray)
+  {
+    ROS_ERROR("Failed to get tau g scales, unexpected type.");
+    return false;
+  }
+  for (int i = 0; i < tau_g_scale.size(); ++i)
+  {
+    if (tau_g_scale[i].getType() != XmlRpc::XmlRpcValue::TypeDouble)
+    {
+      ROS_ERROR("Failed to get tau g scales, unexpected type in array.");
+      return false;
+    }
+    tau_g_scale_.push_back(static_cast<double>(tau_g_scale[i]));
   }
 
   if (!model_.initParam("robot_description"))
@@ -53,39 +72,100 @@ bool GravityCompensationController::init(hardware_interface::EffortJointInterfac
   }
 
   joint_positions_.resize(chain_.getNrOfJoints());
+  joint_positions_.data.setZero();
+  tau_g_.resize(chain_.getNrOfJoints());
+  tau_g_.data.setZero();
 
   KDL::Vector gravity{0.0, 0.0, -9.81};
   solver_ = std::make_unique<KDL::ChainDynParam>(chain_, gravity);
 
+  pos_hold_server_ = controller_nh.advertiseService("position_hold", &GravityCompensationJointTrajectoryController::positionHoldCallback, this);
+
   return true;
 }
 
-void GravityCompensationController::update(const ros::Time& time, const ros::Duration& period)
+void GravityCompensationJointTrajectoryController::update(const ros::Time& time, const ros::Duration& period)
 {
   JointTrajectoryController::update(time, period);
   computeGravityTorques();
-  for (size_t i = 0; i < joints_.size(); ++i)
+
+  bool desired = position_hold_.load();
+  if (desired != last_position_hold_)
   {
-    double preset_torque = joints_[i].getCommand();
-    joints_[i].setCommand(preset_torque + gravity_torques_[i]);
+    if (desired)
+    {
+      JointTrajectoryController::starting(time);
+      ROS_INFO("GravityCompensationController: hold enabled, command refreshed.");
+    }
+    last_position_hold_ = desired;
+  }
+  
+  if (desired)
+  {
+    for (size_t i = 0; i < joints_.size(); ++i)
+    {
+      double preset_torque = joints_[i].getCommand();
+      joints_[i].setCommand(preset_torque + gravity_torques_[i]);
+    }
+  }
+  else
+  {
+    for (size_t i = 0; i < joints_.size(); ++i)
+    {
+      joints_[i].setCommand(gravity_torques_[i]);
+    }
   }
 }
 
-void GravityCompensationController::computeGravityTorques()
+void GravityCompensationJointTrajectoryController::computeGravityTorques()
 {
-  int i = 0;
+  int idx = 0;
   for (auto& joint : joints_)
   {
-    joint_positions_(i) = joint.getPosition();
-    ++i;
+    joint_positions_(idx) = joint.getPosition();
+    ++idx;
   }
-  KDL::JntArray tau_g;
-  solver_->JntToGravity(joint_positions_, tau_g);
-  gravity_torques_.resize(tau_g.rows());
 
-  for (int j = 0; j < tau_g.rows(); ++j)
+  // Debug
+  int ret = solver_->JntToGravity(joint_positions_, tau_g_);
+  ROS_INFO_STREAM_THROTTLE(1.0, "JntToGravity ret = " << ret);
+  std::ostringstream ss;
+  // Debug
+  
+  gravity_torques_.resize(tau_g_.rows());
+  for (int i = 0; i < tau_g_.rows(); ++i)
   {
-    gravity_torques_[j] = tau_g(j) + (offset_torques_.size() > j ? offset_torques_[j] : 0.0);
+    gravity_torques_[i] = tau_g_(i) * (tau_g_scale_.size() > i ? tau_g_scale_[i] : 1.0)
+      + (offset_torques_.size() > i ? offset_torques_[i] : 0.0);
+    // Debug
+    ss << gravity_torques_[i] << (i + 1 < tau_g_.rows() ? ", " : "");
+    // Debug
   }
+  // Debug
+  ROS_INFO_THROTTLE(1.0, "Gravity torques: [%s]", ss.str().c_str());
+  std::ostringstream qss;
+  for (int i = 0; i < joint_positions_.rows(); ++i)
+  {
+    qss << joint_positions_(i);
+    if (i + 1 < joint_positions_.rows())
+      qss << ", ";
+  }
+  ROS_INFO_THROTTLE(1.0, "Joint positions: [%s]", qss.str().c_str());
+  // Debug
 }
+
+bool GravityCompensationJointTrajectoryController::positionHoldCallback(std_srvs::SetBool::Request& req,
+  std_srvs::SetBool::Response& res)
+{
+  position_hold_.store(req.data);
+  res.success = true;
+  if(req.data)
+    res.message = "Position hold enabled.";
+  else
+    res.message = "Position hold disabled.";
+  return true;
+}
+
 } // namespace gravity_compensation_controller
+
+PLUGINLIB_EXPORT_CLASS(gravity_compensation_controller::GravityCompensationJointTrajectoryController, controller_interface::ControllerBase)
